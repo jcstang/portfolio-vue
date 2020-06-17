b8e074e3580de803b88bf5e951702a3b7e4e31a4
Jacob Stanger <jstangwork@gmail.com>
2020-06-16 18:21:34 -0600

    add picture and nav and header comps

---
+++ src/components/Header.vue
@@ -0,0 +1,92 @@
+<template>
+  <section className="header-section">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3">
+          <img
+            className="align-self-start mr-3"
+            src="{headshot}"
+            alt="Jacob Stanger portrait"
+          />
+        </div>
+        <div className="col-md-9">
+          <h5 className="title is-2">Jacob Stanger</h5>
+          <h5 className="title is-3">About me</h5>
+          <p>
+            Full stack web developer with a passion for iOS development.
+            Educated in computer science at Weber State University and recently
+            earned a coding certificate from the University of Utah. Life-long
+            learner that thrives in team environments and is driven by complex
+            problem solving with more than 100 hours on Udemy and Pluralsight.
+            Expertise interfacing and scoping solutions with clients and clearly
+            explaining technical topics to stakeholders.
+          </p>
+          <hr />
+          <p><strong>Email</strong> JacobcStanger@gmail.com</p>
+          <div className="row container is-fluid">
+            <a
+              className="button is-link social-btn"
+              href="https://www.linkedin.com/in/jacob-stanger/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="icon"><i className="fab fa-linkedin"></i></span>
+              <span>LinkedIn</span>
+            </a>
+
+            <a
+              className="button is-link social-btn"
+              href="https://github.com/jcstang"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="icon"><i className="fab fa-github"></i></span>
+              <span>GitHub</span>
+            </a>
+
+            <a
+              className="button is-info social-btn"
+              href="https://twitter.com/jc_stang"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="icon"><i className="fab fa-twitter"></i></span>
+              <span>Twitter</span>
+            </a>
+            <div className="container">
+              <img
+                src="../../public/badge-coding-bootcamp-trilogy.png"
+                alt="developer certificate"
+                className="badge-img"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {},
+};
+</script>
+
+<style scoped>
+.header-section {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  margin-bottom: 2rem;
+}
+
+.social-btn {
+  margin-bottom: 1rem;
+  margin-right: 1rem;
+}
+
+.badge-img {
+  max-width: 12vw;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <img src="{logo}" className="App-logo" alt="logo" />
+      <h1>Jacob Stanger</h1>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  props: {},
+};
+</script>
